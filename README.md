f3e02c5dec2d4d0298b04dfaf314f504849b4ea5
andlo <andlo@outlook.dk>
2018-12-09 11:41:47 +0000

    added more notes and guitar

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-# <img src='https://rawgithub.com/FortAwesome/Font-Awesome/master/advanced-options/raw-svg/solid/music.svg' card_color='#113AF1' width='50' height='50' style='vertical-align:bottom'/> Sound tuning
-This skill plays a sound in the given note.
+# <img src='https://rawgithub.com/FortAwesome/Font-Awesome/master/advanced-options/raw-svg/solid/music.svg' card_color='#113AF1' width='50' height='50' style='vertical-align:bottom'/> Sound tuner
+This skill plays a sound in the given note or an sound in the given guitar string.
 
 ## About
-This skill plays a sound in the given note. This can be used to tune a musical instruments.
+This skill plays a sound in the given note or an sound in the given guitar string.
+This can be used to tune a musical instruments.
+
+Mycroft is using the even tempered (aka equal tempered) scale, where an octave is a
+frequency ratio of exactly two and a semitone is a frequency ratio of exactly the
+twelfth root of two. In the real world however many different temperaments may be used
+ - see en.wikipedia.org/wiki/Musical_temperament - and octaves too can vary in size, see
+  en.wikipedia.org/wiki/Stretched_octave.
+
+Also Mycroft call middle C for "C4" : this is the commonest octave numbering but some
+people call middle C for "C3" or even for "C5".
 
 
 ## Examples
- - "Gime me an C"
+ - "Gime me an C note"
+ - "Give me an guitar Low E string"
+ - "Give me an A4 note"
+ - "Give me an guitar A string"
 
 
 ## Credits
@@ -16,11 +29,11 @@ Andreas Lorensen (@andlo)
 ## Supported Devices
 platform_mark1 platform_mark2 platform_picroft platform_plasmoid
 
+
 ## Category
 **Information**
+
 
 ## Tags
 #sound
 #tuner
-
-
--- __init__.py
+++ __init__.py
@@ -13,7 +13,8 @@ class SoundTuner(MycroftSkill):
 
     def initialize(self):
         # https://www.seventhstring.com/resources/notefrequencies.html
-        self.NOTES = {'C': 261.6,
+        self.NOTES = {'C middle': 261.6,
+                      'C': 261.6,
                       'C#': 277.2,
                       'D': 293.7,
                       'D#': 311.1,
@@ -24,7 +25,76 @@ class SoundTuner(MycroftSkill):
                       'G#': 415.3,
                       'A': 440.0,
                       'A#': 466.2,
-                      'B': 493.9}
+                      'B': 493.9,
+                      'C0': 16.35,
+                      'C#0': 17.32,
+                      'D0': 18.35,
+                      'D#0': 19.45,
+                      'E0': 20.60,
+                      'F0': 21.83,
+                      'F#0': 23.12,
+                      'G0': 24.50,
+                      'G#0': 25.96,
+                      'A0': 27.50,
+                      'A#0': 29.14,
+                      'B0': 30.87,
+                      'C1': 32.70,
+                      'C#1': 34.65,
+                      'D1':	36.71,
+                      'D#1': 38.89,
+                      'E1':	41.20,
+                      'F1':	43.65,
+                      'F#1': 46.25,
+                      'G1':	49.00,
+                      'G#1': 51.91,
+                      'A1':	55.00,
+                      'A#1': 58.27,
+                      'B1':	61.74,
+                      'C2': 65.41,
+                      'C#2': 69.30,
+                      'D2':	73.42,
+                      'D#2': 77.78,
+                      'E2':	82.41,
+                      'F2':	87.31,
+                      'F#2': 92.50,
+                      'G2':	98.00,
+                      'G#2': 103.8,
+                      'A2':	110.0,
+                      'A#2': 116.5,
+                      'B2': 123.5,
+                      'C3': 130.8,
+                      'C#3': 138.6,
+                      'D3':	146.8,
+                      'D#3': 155.6,
+                      'E3':	164.8,
+                      'F3':	174.6,
+                      'F#3': 185.0,
+                      'G3':	196.0,
+                      'G#3': 207.7,
+                      'A3':	220.0,
+                      'A#3': 233.1,
+                      'B3': 246.9,
+                      'C4': 261.6,
+                      'C#4': 277.2,
+                      'D4': 293.7,
+                      'D#4': 311.1,
+                      'E4': 329.6,
+                      'F4': 349.2,
+                      'F#4': 370.0,
+                      'G4': 392.0,
+                      'G#4': 415.3,
+                      'A4': 440.0,
+                      'A#4': 466.2,
+                      'B4': 493.9
+                      }
+
+        self.GUITAR = {'Low E': 'E2',
+                       'A': 'A3',
+                       'D': 'D4',
+                       'G': 'G4',
+                       'B': 'B4',
+                       'High E': 'E5'
+                       }
 
     @intent_file_handler('tuner.sound.intent')
     def handle_tuner_sound(self, message):
@@ -33,11 +103,22 @@ class SoundTuner(MycroftSkill):
             self.speak_dialog('tuner.sound', data=response, wait=True)
             self.make_sound(message.data.get("note"))
         else:
-            self.speak_dialog('can_not_do.sound', data=response, wait=True)
+            self.speak_dialog('can_not_do.sound', data=response, wait=False)
+
+    @intent_file_handler('guitar.intent')
+    def handle_guitar(self, message):
+        response = {'string': message.data.get("string")}
+        if self.GUITAR[message.data.get("string")]:
+            self.speak_dialog('guitar', data=response, wait=True)
+            string = self.GUITAR[message.data.get("string")]
+            self.make_sound(string)
+        else:
+            self.speak_dialog('can_not_do.sound', data=response, wait=False)
+
 
     def make_sound(self, note):
-        sampleRate = 44100.0  # hertz
-        duration = 2.0        # seconds
+        sampleRate = 48000.0  # hertz
+        duration = 2.0
         frequency = self.NOTES[note]
 
         wavef = wave.open('sound.wav', 'w')
@@ -51,8 +132,8 @@ class SoundTuner(MycroftSkill):
             wavef.writeframesraw(data)
 
         wavef.close()
-        play_wav('sound.wav',)
-        time.sleep(2)
+        play_wav('sound.wav')
+        time.sleep(duration)
         os.remove('sound.wav')
 
 
